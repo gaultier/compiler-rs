853a1120328f7ecc152ceaff9dae438506210dc4
Philippe Gaultier <philippe.gaultier@ory.sh>
2025-12-07 22:13:18 +0100

    [wip]

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,14 @@
+pub mod ast;
+pub mod error;
+pub mod lex;
+mod origin;
+
+use memmap::MmapMut;
 use miniserde::Serialize;
+use std::alloc::GlobalAlloc;
+use std::alloc::Layout;
 use std::ptr::null_mut;
 use std::sync::atomic::{AtomicUsize, Ordering::Relaxed};
-use std::{
-    alloc::{GlobalAlloc, Layout},
-    cell::UnsafeCell,
-};
 
 use crate::{
     ast::{Node, Parser},
@@ -13,22 +17,33 @@ use crate::{
     origin::FileId,
 };
 
-pub mod ast;
-pub mod error;
-pub mod lex;
-mod origin;
-
 const ARENA_SIZE: usize = 1 * 1024 * 1024;
 
 struct SimpleAllocator {
-    arena: UnsafeCell<[u8; ARENA_SIZE]>,
-    remaining: AtomicUsize,
+    base_addr: std::cell::OnceCell<usize>,
+    offset: AtomicUsize,
+}
+
+impl SimpleAllocator {
+    #[cfg(target_arch = "wasm32")]
+    fn os_alloc(&self) -> usize {
+        core::arch::wasm32::memory_grow(0, ARENA_SIZE);
+        0
+    }
+
+    #[cfg(not(target_arch = "wasm32"))]
+    fn os_alloc(&self) -> usize {
+        let mut mmapped = MmapMut::map_anon(ARENA_SIZE).unwrap();
+        let base_addr = mmapped.as_mut_ptr() as usize;
+        std::mem::forget(mmapped);
+        base_addr
+    }
 }
 
 #[global_allocator]
 static ALLOCATOR: SimpleAllocator = SimpleAllocator {
-    arena: UnsafeCell::new([0x55; ARENA_SIZE]),
-    remaining: AtomicUsize::new(ARENA_SIZE),
+    base_addr: std::cell::OnceCell::new(),
+    offset: AtomicUsize::new(0),
 };
 
 unsafe impl Sync for SimpleAllocator {}
@@ -38,32 +53,40 @@ unsafe impl GlobalAlloc for SimpleAllocator {
         let size = layout.size();
         let align = layout.align();
 
-        let align_mask_to_round_down = !(align - 1);
-        assert!(align <= 8);
+        // Once.
+        let base_addr = *self.base_addr.get_or_init(|| {
+            let res = self.os_alloc();
+            res
+        });
 
         let mut allocated = 0;
         if self
-            .remaining
-            .fetch_update(Relaxed, Relaxed, |mut remaining| {
-                if size > remaining {
+            .offset
+            .fetch_update(Relaxed, Relaxed, |mut offset| {
+                let padding = (0usize).wrapping_sub(offset) & (align - 1);
+                assert!(padding <= align);
+
+                if padding + offset + size > ARENA_SIZE {
                     return None;
                 }
-                remaining -= size;
-                remaining &= align_mask_to_round_down;
-                allocated = remaining;
-                Some(remaining)
+                allocated = offset + padding;
+                offset += padding + size;
+                Some(offset)
             })
             .is_err()
         {
             return null_mut();
         };
-        unsafe { self.arena.get().cast::<u8>().add(allocated) }
+        unsafe { (base_addr as *mut u8).add(allocated) }
     }
 
     unsafe fn dealloc(&self, _ptr: *mut u8, _layout: Layout) {}
 }
 
-pub fn arena_alloc() {}
+#[unsafe(no_mangle)]
+pub extern "C" fn dealloc() {
+    todo!()
+}
 
 #[unsafe(no_mangle)]
 pub extern "C" fn alloc_u8(size: u32) -> usize {
@@ -73,30 +96,15 @@ pub extern "C" fn alloc_u8(size: u32) -> usize {
     ptr as usize
 }
 
-#[unsafe(no_mangle)]
-pub extern "C" fn dealloc() {
-    todo!()
-}
-
 #[repr(transparent)]
 pub struct AllocHandle(u64);
 
-#[unsafe(no_mangle)]
-pub extern "C" fn lex(in_ptr: *const u8, in_len: usize, file_id: FileId) -> AllocHandle {
-    assert!(!in_ptr.is_null());
-
-    let input_bytes = unsafe { &*std::ptr::slice_from_raw_parts(in_ptr, in_len) };
-    let input_str = std::str::from_utf8(input_bytes).unwrap();
-
-    let mut lexer = Lexer::new(file_id);
-    lexer.lex(input_str);
-
-    let json = miniserde::json::to_string(&(&lexer.tokens, &lexer.errors));
-
-    let ptr = json.as_bytes().as_ptr() as u64;
-    let len = json.len() as u32 as u64;
-
-    AllocHandle(ptr << 32 | len)
+impl AllocHandle {
+    fn unpack(&self) -> (u32, u32) {
+        let ptr = (self.0 << 32) as u32;
+        let len = (self.0 & 0xff_ff_ff_ff) as u32;
+        (ptr, len)
+    }
 }
 
 #[derive(Serialize)]
@@ -137,14 +145,18 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_lex() {
+    fn test_c_api() {
         let input = " 123 456 ";
-        let handle = lex(input.as_ptr(), input.len(), 1);
-        let (fat_ptr, _data) = Allocation::from_wasm_memory_handle(handle);
-        assert!(fat_ptr.len > 0);
-        assert!(fat_ptr.cap > 0);
-        assert!(fat_ptr.align == 8);
+        let handle = parse(input.as_ptr(), input.len(), 1);
+        let (ptr, len) = handle.unpack();
+        println!("{} {}", ptr, len);
+        assert!(ptr > 0);
+        assert!(len > 0);
 
-        dealloc(handle);
+        let bytes = unsafe { std::slice::from_raw_parts(ptr as usize as *const u8, len as usize) };
+        let s = std::str::from_utf8(bytes).unwrap();
+        assert!(s.len() > 0);
+
+        //dealloc(handle);
     }
 }
